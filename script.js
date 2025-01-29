030513163098b7392756bff3fd489e3d5c12afca
SathvikDixit <sathvikdixit21@gmail.com>
2025-01-29 23:35:02 +0530

    learnt about spread, data types etc

--- script.js
+++ script.js
@@ -2,25 +2,47 @@
 
 // Console has 3 types i. log      ii. warn     iii. error
 // Console.log();
-console.log("hellow");
+    // console.log("hellow");
 
 // Console.warn();
-console.warn("Sathivk Dixit");
+    // console.warn("Sathivk Dixit");
 
 // Console.error();
-console.error("Tiger");
+    // console.error("Tiger");
 
 
 // Alert
-alert("You are on the wrong way b!tc#");
+    // alert("You are on the wrong way b!tc#");
 
 
 // Pronpt, Alert ka bhai hain but vo ek kadam aage hain
-prompt("What do you do?");
+    // prompt("What do you do?");
 
 
 
 
-var arr = [1,4,2,3,6,7,8];
-b = [...arr];
+    // var arr = [1,4,2,3,6,7,8];
+    // b = [...arr];
 // b.pop();
+
+
+// if(12>19) {
+//     console.log("Hey!");
+// }
+// else {
+//     console.log("hi hello");
+// }
+
+
+// for(var i = 0; i<=4; i++) {
+//     console.log("it's 5 of em");
+// }
+
+
+
+function asdf() {
+    console.log("Sathvik Dixit");
+    console.log("Pushpa Raj (w/o surname)");
+    console.log("Banwar Singh Shikawat ");
+}
+asdf();
