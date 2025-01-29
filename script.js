2893a37bca2b58be15606661f5fc2c5e56e78fc9
SathvikDixit <sathvikdixit21@gmail.com>
2025-01-29 16:44:11 +0530

    learnt about console, alert and prompt

---
+++ script.js
@@ -0,0 +1,19 @@
+// Console Alert Prompt 
+
+// Console has 3 types i. log      ii. warn     iii. error
+// Console.log();
+console.log("hellow");
+
+// Console.warn();
+console.warn("Sathivk Dixit");
+
+// Console.error();
+console.error("Tiger");
+
+
+// Alert
+alert("You are on the wrong way b!tc#");
+
+
+// Pronpt, Alert ka bhai hain but vo ek kadam aage hain
+prompt("What do you do?");
